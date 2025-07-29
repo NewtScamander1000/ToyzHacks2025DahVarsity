9d5f3ffb6017ae2031689ca274ee15909d2a5a5a
NewtScamander1000 <comediancam@gmail.com>
2025-07-29 14:45:55 -0400

    Update to scripts

--- Assets/controller.cs
+++ Assets/controller.cs
@@ -8,27 +8,132 @@ public class controller : MonoBehaviour
     // Start is called before the first frame update
     public GameObject Effect1;
     public GameObject Effect2;
-
+    private bool fast;
+    private bool music;
+    private bool isidle;
+    private bool quickjump;
+    private bool walk;
+    private bool sword;
+    private bool attack;
     void Start()
     {
         animator = GetComponent<Animator>();
         Debug.Log(animator);
+        //Start the coroutine we define below named ExampleCoroutine.
+        fast = animator.GetBool("fast");
+        music = animator.GetBool("music");
+        isidle = animator.GetBool("isidle");
+        quickjump = animator.GetBool("quickjump");
+        walk = animator.GetBool("walk");
+        sword = animator.GetBool("sword");
+        attack = animator.GetBool("attack");
     }
+
+    IEnumerator ExampleCoroutine()
+    {
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+        animator.SetBool("isidle", true);
+        yield return new WaitForSeconds(1f);
+        Debug.Log("Waited for 3 seconds.");
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        animator.SetBool("isidle", false);
+
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+    }
+    IEnumerator runjump()
+    {
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+        animator.SetBool("quickjump", true);
+        yield return new WaitForSeconds(0.6f);
+        Debug.Log("Waited for 3 seconds.");
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        animator.SetBool("quickjump", false);
+
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+    }
+        IEnumerator swordy()
+    {
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+        animator.SetBool("attack", true);
+        yield return new WaitForSeconds(0.6f);
+        Debug.Log("Waited for 3 seconds.");
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        animator.SetBool("attack", false);
+
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+    }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        bool isdancing = animator.GetBool("isdancing");
-        if (Input.GetKeyDown(KeyCode.Space)) // Check if space key is pressed down
+
+        //bool isidle = animator.GetBool("isidle");
+
+
+        if (Input.GetKey(KeyCode.Space))
         {
-            if (!isdancing) // Start  dancing only if not already dancing
+            StartCoroutine(ExampleCoroutine());
+        }
+                if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetBool("walk", true);
+        }
+        else
+        {
+            animator.SetBool("walk", false);
+        }
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            animator.SetBool("sword", true);
+            Debug.Log("" + Time.time);
+        }
+        if (Input.GetKey(KeyCode.F))
+        {
+                StartCoroutine(swordy());
+        }
+        //else
+            //{
+            //    animator.SetBool("isidle", false);
+            //}
+            // Check if the Space key is currently held down
+            if (Input.GetKey(KeyCode.LeftShift))
             {
-                animator.SetBool("isdancing", true);
-                //Effect.SetActive(true);
+                animator.SetBool("fast", true);
             }
+            else
+            {
+                animator.SetBool("fast", false);
+            }
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.Space))
+        {
+            StartCoroutine(runjump());
         }
 
-        if (isdancing && Input.GetKey("c"))
+        if (Input.anyKeyDown) // Check if any key is pressed down
+            {
+                if (fast) // Stop dancing only if currently dancing
+                {
+                    animator.SetBool("fast", false);
+                    //animator.SetBool("isidle", false);
+                    if (Effect2.activeSelf)
+                    {
+                        Effect2.SetActive(false);
+                    }
+                    if (Effect1.activeSelf)
+                    {
+                        Effect1.SetActive(false);
+                    }
+                }
+            }
+        if (fast && Input.GetKey("c"))
         {
             if (Effect2.activeSelf)
             {
@@ -36,7 +141,7 @@ public class controller : MonoBehaviour
             }
             Effect1.SetActive(true);
         }
-        if (isdancing && Input.GetKey("b"))
+        if (fast && Input.GetKey("b"))
         {
             Debug.Log(Effect1);
             if (Effect1.activeSelf)
@@ -46,21 +151,8 @@ public class controller : MonoBehaviour
             }
             Effect2.SetActive(true);
         }
-        else if (Input.anyKeyDown) // Check if any key is pressed down
-        {
-            if(isdancing) // Stop dancing only if currently dancing
-            {
-                animator.SetBool("isdancing", false);
-                if (Effect2.activeSelf){
-                    Effect2.SetActive(false);
-                }
-                if (Effect1.activeSelf){
-                    Effect1.SetActive(false);
-                }
-            }     
-        }
 
-    
-    
-}
+
+
+    }
 }
